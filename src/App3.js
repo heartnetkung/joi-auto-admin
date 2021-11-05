687331705841d064b624476cd62f842f041cff01
heartnetkung <heartnetkung@gmail.com>
2021-11-05 16:49:13 +0700

    refactor

--- src/App3.js
+++ src/App3.js
@@ -4,12 +4,13 @@ import AutoAdmin from "./controller";
 const wait = (ms) => new Promise((res) => setTimeout(res, ms));
 
 const App = () => {
-	const api = {
+	const props = {
+		name: "ลูกค้า",
 		getMany: async () => {
 			const data = [];
 			for (let i = 0; i < 100; i++)
 				data.push({
-					_id: i ,
+					_id: i,
 					name: `Edward King ${i}`,
 					purchased_value: 3000 + Math.round(10 * Math.random()),
 					district: ["กรุงเทพมหานคร", "ยานนาวา"],
@@ -26,36 +27,27 @@ const App = () => {
 		deleteMany: async (a) => {
 			await wait(1000);
 		},
+		schema: Joi.object({
+			name: Joi.string()
+				.min(3)
+				.max(30)
+				.required()
+				.label("ชื่อ")
+				.meta({ placeholder: "ชื่อภาษาไทย" }),
+			purchased_value: Joi.number().integer().label("เงิน"),
+			district: Joi.array()
+				.label("เขต")
+				.meta({ fieldType: "AddressDistrict" }),
+			create_date: Joi.date()
+				.default(Date.now)
+				.label("วันสมัคร")
+				.meta({ disabled: true }),
+		}),
 	};
-
-	const actions = [{ label: "tor", onClick: (a) => console.log(a) }];
-
-	const schema = Joi.object({
-		name: Joi.string()
-			.min(3)
-			.max(30)
-			.required()
-			.label("ชื่อ")
-			.meta({ placeholder: "ชื่อภาษาไทย" }),
-		
-		purchased_value: Joi.number().integer().label("เงิน"),
-		district: Joi.array()
-			.label("เขต")
-			.meta({ fieldType: "AddressDistrict" }),
-		create_date: Joi.date()
-			.default(Date.now)
-			.label("วันสมัคร")
-			.meta({ disabled: true }),
-	});
 
 	return (
 		<center style={{ maxWidth: 800, margin: "auto", marginTop: 40 }}>
-			<AutoAdmin
-				{...api}
-				name="ลูกค้า"
-				schema={schema}
-				// actions={actions}
-			/>
+			<AutoAdmin {...props} />
 		</center>
 	);
 };
