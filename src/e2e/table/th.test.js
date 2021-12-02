a32ad69811d87f59f67d498471583ab56a481365
heartnetkung <heartnetkung@gmail.com>
2021-12-02 23:34:01 +0700

    add testing for th

---
+++ src/e2e/table/th.test.js
@@ -0,0 +1,114 @@
+import { render, fireEvent, screen, wait } from "../boilerplate";
+import { AutoAdmin, Joi } from "../../lib";
+import { prop1 } from "../mock";
+import React from "react";
+
+const getColumnButtons = async () => {
+	await screen.findByText(/fooName/);
+	return {
+		filters: document.querySelectorAll(".ant-table-filter-trigger"),
+		sortUps: document.querySelectorAll(".ant-table-column-sorter-up"),
+		sortDowns: document.querySelectorAll(".ant-table-column-sorter-down"),
+	};
+};
+
+it("basic case", async () => {
+	var getMany = () => [{ foo: "1" }, { foo: "2" }];
+	render(<AutoAdmin {...prop1} getMany={getMany} />);
+	var { filters, sortUps, sortDowns } = await getColumnButtons();
+	expect(filters.length).toBe(1);
+	expect(sortUps.length).toBe(1);
+	expect(sortDowns.length).toBe(1);
+	var allRows = document.querySelectorAll("tr");
+	expect(allRows.length).toBe(4);
+	expect(allRows[2].textContent).toMatch("1");
+	expect(allRows[3].textContent).toMatch("2");
+
+	fireEvent.click(sortUps[0]);
+	await wait(10);
+	var allRows = document.querySelectorAll("tr");
+	expect(allRows.length).toBe(4);
+	expect(allRows[2].textContent).toMatch("2");
+	expect(allRows[3].textContent).toMatch("1");
+
+	fireEvent.click(sortDowns[0]);
+	await wait(10);
+	var allRows = document.querySelectorAll("tr");
+	expect(allRows.length).toBe(4);
+	expect(allRows[2].textContent).toMatch("1");
+	expect(allRows[3].textContent).toMatch("2");
+
+	fireEvent.click(filters[0]);
+	await wait(10);
+	var dropdown = document.querySelectorAll(
+		".ant-table-filter-dropdown [type='checkbox']"
+	);
+	fireEvent.click(dropdown[1]);
+	await wait(10);
+	var button = document.querySelector(
+		".ant-table-filter-dropdown .ant-btn-primary"
+	);
+	fireEvent.click(button);
+	await wait(10);
+	var allRows = document.querySelectorAll("tr");
+	expect(allRows.length).toBe(3);
+	expect(allRows[2].textContent).toMatch("2");
+	// test warning
+	// expect(console.error.mock.calls.length).toBe(0);
+});
+
+it("disableSorting", async () => {
+	var schema = Joi.object({
+		foo: Joi.string().label("fooName").meta({ disableSorting: true }),
+	});
+	render(<AutoAdmin {...prop1} schema={schema} />);
+	var { sortUps, sortDowns } = await getColumnButtons();
+	expect(sortUps.length).toBe(0);
+	expect(sortDowns.length).toBe(0);
+	expect(console.error.mock.calls.length).toBe(0);
+});
+
+it("disableFilter", async () => {
+	var schema = Joi.object({
+		foo: Joi.string().label("fooName").meta({ disableFilter: true }),
+	});
+	render(<AutoAdmin {...prop1} schema={schema} />);
+	var { filters } = await getColumnButtons();
+	expect(filters.length).toBe(0);
+	expect(console.error.mock.calls.length).toBe(0);
+});
+
+it("cellWidth", async () => {
+	var schema = Joi.object({
+		foo: Joi.string().label("fooName").meta({ cellWidth: 112 }),
+		hello: Joi.string().label("helloName"),
+	});
+	render(<AutoAdmin {...prop1} schema={schema} />);
+	await screen.findByText("fooName");
+	var col = document.querySelectorAll("col");
+	expect(col.length).toBe(1);
+	expect(col[0].style.width).toMatch("112px");
+	expect(console.error.mock.calls.length).toBe(0);
+});
+
+it("cellHide", async () => {
+	var schema = Joi.object({
+		foo: Joi.string().label("fooName").meta({ cellHide: true }),
+		hello: Joi.string().label("helloName"),
+	});
+	render(<AutoAdmin {...prop1} schema={schema} />);
+	await screen.findByText("helloName");
+	expect(screen.queryByText("fooName")).toBe(null);
+	expect(console.error.mock.calls.length).toBe(0);
+});
+
+it("cellShow", async () => {
+	var schema = Joi.object({
+		foo: Joi.string().label("fooName").meta({ cellShow: true }),
+		hello: Joi.string().label("helloName"),
+	});
+	render(<AutoAdmin {...prop1} schema={schema} />);
+	await screen.findByText("fooName");
+	expect(screen.queryByText("helloName")).toBe(null);
+	expect(console.error.mock.calls.length).toBe(0);
+});
