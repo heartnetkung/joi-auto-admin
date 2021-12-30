67119f45bec2af19a31f875a147758df4e832134
heartnetkung <heartnetkung@gmail.com>
2021-12-30 12:21:14 +0700

    remove unwanted padding

--- src/editor/screens/editor-tool/right-panel-views/code-blocks.js
+++ src/editor/screens/editor-tool/right-panel-views/code-blocks.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import PropTypes from "prop-types";
 import { CopyBlock, dracula } from "react-code-blocks";
 
 const RenderCodeBlocks = (props) => {
   const { text } = props;
-  return <CopyBlock text={text} language="jsx" theme={dracula} />;
+
+  //hack: remove unwanted style from child node
+  const container = useRef(null);
+  useEffect(() => {
+    if (!container.current) return;
+    var span = container.current.querySelector(":scope>div>span");
+    if (span) span.style.padding = null;
+  }, [container]);
+
+  return (
+    <div ref={container}>
+      <CopyBlock text={text} language="jsx" theme={dracula} />
+    </div>
+  );
 };
 
 RenderCodeBlocks.propTypes = {
