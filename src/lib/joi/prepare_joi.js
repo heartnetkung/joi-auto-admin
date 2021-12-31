f14bbd12516149be4ea0d467112f75c974477450
heartnetkung <heartnetkung@gmail.com>
2021-12-31 18:11:33 +0700

    allow null fields in optional

--- src/lib/joi/prepare_joi.js
+++ src/lib/joi/prepare_joi.js
@@ -3,53 +3,63 @@ import Joi from "joi/lib/index";
 // https://stackoverflow.com/questions/42370881/allow-string-to-be-null-or-empty-in-joi-and-express-validation
 // https://github.com/sideway/joi/issues/482
 export const fixEmptyString = (joiObj, describeObj) => {
-	const traverse = (node, path, ans) => {
-		if (node?.flags?.label && node?.type === "string")
-			if (node?.flags?.presence !== "required") ans.push(path);
+	const strPaths = [];
+	const traverse = (node, path) => {
+		if (node?.flags?.label && node?.flags?.presence !== "required")
+			if (node?.type === "string") strPaths.push(path);
 
 		var keys = node.keys;
-		if (keys)
-			for (var key in keys) traverse(keys[key], [...path, key], ans);
+		if (keys) for (var key in keys) traverse(keys[key], [...path, key]);
 	};
 
-	var paths = [];
-	traverse(describeObj, [], paths);
-	return joiObj.fork(paths, (schema) => schema.allow(""));
+	traverse(describeObj, []);
+	return joiObj.fork(strPaths, (schema) => schema.allow("", null));
 };
 
 export const handleDevMode = (joiObj, describeObj) => {
+	const strPaths = [];
+	const otherPaths = [];
 	const traverse = (node, path) => {
 		if (node?.flags?.label && node?.flags?.presence) {
 			delete node.flags.presence;
-			paths.push(path);
+			if (node?.type === "string") strPaths.push(path);
+			else otherPaths.push(path);
 		}
 
 		var keys = node.keys;
 		if (keys) for (var key in keys) traverse(keys[key], [...path, key]);
 	};
 
-	const paths = [];
 	traverse(describeObj, []);
-	return joiObj.fork(paths, (a) => a.optional());
+	return joiObj
+		.fork(otherPaths, (a) => a.optional().allow(null))
+		.fork(strPaths, (a) => a.optional().allow("", null));
 };
 
 export const handleFieldHide = (joiObj, describeObj) => {
-	const traverse = (node, path, ans) => {
-		if (node?.flags?.label && node?.metas?.[0]?.fieldHide) ans.push(path);
+	const strPaths = [];
+	const otherPaths = [];
+	const traverse = (node, path) => {
+		if (node?.flags?.label && node?.metas?.[0]?.fieldHide) {
+			if (node?.type === "string") strPaths.push(path);
+			else otherPaths.push(path);
+		}
 
 		var keys = node.keys;
-		if (keys)
-			for (var key in keys) traverse(keys[key], [...path, key], ans);
+		if (keys) for (var key in keys) traverse(keys[key], [...path, key]);
 	};
 
-	var paths = [];
-	traverse(describeObj, [], paths);
-	return joiObj.fork(paths, (schema) => schema.optional());
+	traverse(describeObj, []);
+	return joiObj
+		.fork(otherPaths, (a) => a.optional().allow(null))
+		.fork(strPaths, (a) => a.optional().allow("", null));
 };
 
 export const prepare = (joiObj, describeObj, devMode) => {
-	joiObj = fixEmptyString(joiObj, describeObj);
 	if (devMode) joiObj = handleDevMode(joiObj, describeObj);
-	joiObj = handleFieldHide(joiObj, describeObj);
+	else {
+		joiObj = fixEmptyString(joiObj, describeObj);
+		joiObj = handleFieldHide(joiObj, describeObj);
+	}
 	return joiObj.append({ _id: Joi.any() });
 };
