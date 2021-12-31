e0d03f447d974966c3857b159e2c9596c9d4ed3f
heartnetkung <heartnetkung@gmail.com>
2021-12-31 12:23:10 +0700

    move fieldArray implementation to setFieldValue

--- src/lib/formik/components/barcode.js
+++ src/lib/formik/components/barcode.js
@@ -1,93 +1,86 @@
 import PropTypes from "prop-types";
-import { Input } from "formik-antd";
-import { useState, useEffect, useRef, memo } from "react";
+import { Input as FInput } from "formik-antd";
 import _ from "lodash";
-import React from "react";
-import { Typography, Divider, Col, Row, Button } from "antd";
+import React, { useState } from "react";
+import { Typography, Divider, Col, Row, Button, Input } from "antd";
 import { CloseOutlined } from "@ant-design/icons";
-import { FieldArray } from "formik";
 import { usePersistFn } from "../../shared/hook";
+import { useFormikContext } from "formik";
 
 const OMIT_FIELDS = ["loadBarcodeName"];
 const { Text } = Typography;
 
 const Barcode = (props) => {
   const { loadBarcodeName, name } = props;
+  const { values, setFieldValue } = useFormikContext();
 
-  const props2 = _.omit(props, OMIT_FIELDS);
-  const [barcodes, setBarcodes] = useState([]);
   const [inputValue, setInputValue] = useState("");
-  const ref = useRef(null);
+  const barcodes = _.get(values, name) || [];
+  const itemNames = _.get(values, "$" + name);
 
   const onKeyDown = usePersistFn(async (event) => {
     if (event.key !== "Enter") return;
-    if (ref.current) ref.current.push(inputValue);
-    setBarcodes((a) => [...a, { inputValue, _id: Math.random() }]);
+    setFieldValue(name, [...barcodes, inputValue], false);
     setInputValue("");
     event.preventDefault();
+    if (itemNames[inputValue]) return;
+
+    var temp;
+    try {
+      var newName = await loadBarcodeName(inputValue);
+      temp = { ...itemNames, [inputValue]: newName || "ไม่พบข้อมูล" };
+      setFieldValue("$" + name, temp, false);
+    } catch (e) {
+      temp = { ...itemNames, [inputValue]: "ไม่พบข้อมูล" };
+      setFieldValue("$" + name, temp, false);
+    }
   });
 
-  const remove = usePersistFn((index) => {
-    if (ref.current) ref.current.remove(index);
-    setBarcodes((a) => a.filter((a, i) => i !== index));
-  });
+  const remove = (index) => {
+    var newValue = _.filter(barcodes, (a, i) => i !== index);
+    setFieldValue(name, newValue, false);
+  };
 
-  const render = usePersistFn((helpers) => {
-    ref.current = helpers;
-    return (
-      <>
-        <Input
-          {...props2}
-          onKeyDown={onKeyDown}
-          validator={null}
-          name={"$" + name}
-          value={inputValue}
-          onChange={(event) => setInputValue(event.target.value)}
+  return (
+    <>
+      <Input
+        {..._.omit(props, OMIT_FIELDS)}
+        onKeyDown={onKeyDown}
+        validator={null}
+        name={null}
+        value={inputValue}
+        onChange={(event) => setInputValue(event.target.value)}
+      />
+      <Divider />
+      {barcodes.map((a, i) => (
+        <BarcodeLine
+          key={i + ""}
+          name={name + `[${i}]`}
+          itemName={itemNames?.[a]}
+          onClick={() => remove(i)}
         />
-        <Divider />
-        {barcodes.map((a, i) => (
-          <BarcodeLine
-            key={a._id}
-            index={i}
-            loadItemName={loadBarcodeName}
-            name={name}
-            remove={remove}
-            value={a.inputValue}
-          />
-        ))}
-      </>
-    );
-  });
-
-  return <FieldArray name={name} render={render} />;
+      ))}
+    </>
+  );
 };
 
 Barcode.propTypes = {
   loadBarcodeName: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
 };
 
-Barcode.defaultProps = {};
-
-const BarcodeLine = memo((props) => {
-  const { index, loadItemName, name, value, remove } = props;
-  const [itemName, setItemName] = useState("");
-
-  /* eslint-disable react-hooks/exhaustive-deps */
-  useEffect(() => {
-    loadItemName(value)
-      .then((a) => setItemName(a || "ไม่พบข้อมูล"))
-      .catch(() => setItemName("ไม่พบข้อมูล"));
-  }, []);
+const BarcodeLine = (props) => {
+  const { name, itemName, onClick } = props;
 
   return (
     <>
       <Row>
         <Col span={8}>
-          <Input
+          <FInput
             disabled={true}
             size="small"
             style={{ marginBottom: 5 }}
-            name={name + `[${index}]`}
+            name={name}
           />
         </Col>
         <Col span={16}>
@@ -96,13 +89,13 @@ const BarcodeLine = memo((props) => {
             size="small"
             shape="circle"
             style={{ marginLeft: 20 }}
-            onClick={() => remove(index)}
+            onClick={onClick}
           />
           <Text style={{ marginLeft: 20 }}>{itemName}</Text>
         </Col>
       </Row>
     </>
   );
-});
+};
 
 export default Barcode;
